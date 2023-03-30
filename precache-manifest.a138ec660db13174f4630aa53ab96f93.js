6e120c7269f3e13c0628de9e8819f6e6de528ac0
verofoxy <veronia.mikhail@epitech.eu>
2023-03-30 17:53:31 +0200

    Updates

---
+++ precache-manifest.a138ec660db13174f4630aa53ab96f93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2554f678e415f68df9aa",
+    "url": "/hackaton-eco-conception/static/css/main.62b1b02e.chunk.css"
+  },
+  {
+    "revision": "2554f678e415f68df9aa",
+    "url": "/hackaton-eco-conception/static/js/main.2554f678.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/hackaton-eco-conception/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/hackaton-eco-conception/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "19e5739a36efb80bf2bd",
+    "url": "/hackaton-eco-conception/static/js/runtime~main.19e5739a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/hackaton-eco-conception/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2be61c4407a4efdd11b8f7b8d08b2e5d",
+    "url": "/hackaton-eco-conception/index.html"
+  }
+];
